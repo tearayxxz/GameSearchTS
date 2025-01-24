64c54250b18cfd43bcb79e3da1a14d12c0c84a42
tearayxxz <dotv55@yahoo.com>
2025-01-24 18:03:27 +0700

    add Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import GameListPage from './pages/GameListPage';
 import GameDetailPage from './pages/GameDetailPage';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/games" element={<GameListPage />} />
         <Route path="/games/:id" element={<GameDetailPage />} />
       </Routes>
+      <Analytics />
     </>
   );
 }
